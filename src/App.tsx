68b08a3260ddfdbfc5ccbb04f51baeaffae00bab
upalinski <ulad@cere.io>
2022-02-21 17:05:35 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, {useCallback} from 'react';
 import './App.css';
 import {styled} from '@mui/material/styles';
-import {Box, Button, ButtonGroup, Modal, TextField, Typography} from '@mui/material';
+import {
+    Box, Button, ButtonGroup, Modal, Paper,
+    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography
+} from '@mui/material';
 import {ContentAddressableStorage, Piece, Scheme} from '@cere-ddc-sdk/content-addressable-storage';
 import {stringToU8a} from '@cere-ddc-sdk/util';
+import FolderIcon from '@mui/icons-material/Folder';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 
 const Input = styled('input')({
     display: 'none',
@@ -20,6 +25,19 @@ const style = {
     boxShadow: 24,
     p: 4,
 };
+
+class File {
+    name: string = ""
+    size: string = ""
+    kind: string = ""
+    dateAdded: string = ""
+}
+
+const rows: Array<File> = [
+    {name: 'photos', size: "0", kind: "Folder", dateAdded: "17.02.2022 at 17:32"},
+    {name: '1628070362800410.pdf', size: "13000000", kind: "File", dateAdded: "15.02.2022 at 9:46"},
+    {name: 'metadata.json', size: "11000", kind: "File", dateAdded: "9.02.2022 at 18:24"},
+];
 
 function App() {
     const [open, setOpen] = React.useState(false);
@@ -42,15 +60,6 @@ function App() {
             };
             const pieceUri = await contentAddressableStorage.store(BigInt(10), piece)
             console.log("Stored piece uri=" + pieceUri.toString())
-
-
-            /*   try {
-                   await sendData({ user: user as User, request, ddcClient, subscriber });
-                   setSubmitting(false);
-                   resetForm();
-               } catch (err) {
-                   setSubmitting(false);
-               }*/
         }, []
         //[ddcClient, resetForm, resetLog, setSubmitting, subscriber, user],
     );
@@ -64,6 +73,36 @@ function App() {
                     Upload
                 </Button>
             </label>
+
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Size</TableCell>
+                            <TableCell align="right">Kind</TableCell>
+                            <TableCell align="right">Date added</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {(row.kind) === "Folder" ? <FolderIcon color="primary"/> :
+                                        <InsertDriveFileIcon color="primary"/>}
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{(row.kind) === "Folder" ? "--" : row.size}</TableCell>
+                                <TableCell align="right">{row.kind}</TableCell>
+                                <TableCell align="right">{row.dateAdded}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
 
             <Modal
                 open={open}
